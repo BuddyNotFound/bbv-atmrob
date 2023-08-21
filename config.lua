d1a88917e3bb5883d6cca54d3da7a49e5ef6087b
BuddyBoyVilla <74051918+BuddyNotFound@users.noreply.github.com>
2023-08-21 16:23:51 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -8,8 +8,8 @@ Config.Settings = {
 	Framework = 'QB', -- QB ONLY
 	Inventory = 'OX', -- QB/OX
 	Target = "OX", -- OX/QB
-	WebHook = "", -- discord webhook
-	ATMs = { -- Props that can be robbed 
+	WebHook = "", -- Discord webhook 
+	ATMs = { -- Props that can be robbed - https://forge.plebmasters.de/
 		'prop_atm_01', 'prop_atm_02', 'prop_fleeca_atm', 'prop_atm_03'
 	},
 	Reward = 500, -- Cash Reward per bill.
